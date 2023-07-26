e224a79b19e0aa323ec2a97a7a0674e97277fa56
Olena573 <linboykomail@gmail.com>
2023-07-26 23:34:27 +0300

    A homework for first lesson of Selenium.

--- src/test/java/MainMethod.java
+++ src/test/java/MainMethod.java
@@ -1,6 +1,9 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class MainMethod {
@@ -10,8 +13,19 @@ public class MainMethod {
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://uk.wikipedia.org/wiki/%D0%93%D0%BE%D0%BB%D0%BE%D0%B2%D0%BD%D0%B0_%D1%81%D1%82%D0%BE%D1%80%D1%96%D0%BD%D0%BA%D0%B0");
+        //I've used the Ukrainian Wikipedia.
+        driver.manage().window().fullscreen();
+        System.out.println (driver.getWindowHandle());
+        //here we saved an id of the window;
+        driver.findElement(By.id("searchInput")).sendKeys("Ubisoft");
+        driver.findElement(By.id("searchInput")).sendKeys(Keys.ENTER);
+        //Here we searched for the Ubisoft;
+
+        driver.findElement(By.xpath("/html/body/div[3]/div[3]/div[4]/div[3]/div[4]/ul/li[1]/table/tbody/tr/td[2]/div[1]/a/span")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 900000);
+
         try {
-            Thread.sleep(3000);
+            Thread.sleep(90000);
         }catch (InterruptedException e){
             throw new RuntimeException(e);
         }
